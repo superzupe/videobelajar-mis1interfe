31cdda47dd5e63eb5aa3df1abda47f3f85b2ec0b
zulfaz <zahrozulfa516@gmail.com>
2025-11-04 22:11:48 +0700

    setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+// import { useState } from 'react'
+
+function App() {
+  return (
+    <h1 className="text-yellow-900 font-bold">Haloooo ini tailwind udah mulai kah</h1>
+  );
+}
+
+export default App;
